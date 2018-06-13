5911e2a2988c2e94907b6462a767fc9117a70a1b
MasbaIUBAT <32599706+MasbaIUBAT@users.noreply.github.com>
2018-06-13 18:44:04 +0600

    Create .js

---
+++ .js
@@ -0,0 +1,18 @@
+function mergeSortedArry(first, second){
+ var merged = [];
+ var length = first.length + second.length;
+ var j = 0;
+ var k = 0;
+ for(var i =0; i<length;i++){
+  if(first[j]<second[k]){
+   merged.push(first[j]);
+   j++;
+  }
+  else{
+  merged.push(second[k]);
+  k++;
+  }
+ }
+ return merged;
+}
+mergeSortedArry([3,5,6,9],[1,2,3,29]);
